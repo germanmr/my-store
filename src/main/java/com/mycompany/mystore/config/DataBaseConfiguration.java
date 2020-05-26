7a242aae89e7db75d9affa0ce05c9a121ca4f4a9
German Muñoz <german.munioz.romano@gmail.com>
2020-05-26 17:46:51 -0300

    -) docker-compose file cleaned
    -) We use the RedisProperties class to get properties from apllication.properties
    
    -) Add up-instance bash file for easier testing

--- src/main/java/com/mycompany/mystore/config/DataBaseConfiguration.java
+++ src/main/java/com/mycompany/mystore/config/DataBaseConfiguration.java
@@ -1,7 +1,9 @@
 package com.mycompany.mystore.config;
 
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
@@ -10,50 +12,51 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
 @EnableRedisRepositories
 public class DataBaseConfiguration {
 
-    // TODO add @Value to the application properties file!!
-
-//    @Bean
-//    JedisConnectionFactory jedisConnectionFactory() {
-//        return new JedisConnectionFactory();
-//    }
-//
-//    @Bean
-//    public RedisTemplate<String, Object> redisTemplate() {
-//        RedisTemplate<String, Object> template = new RedisTemplate<>();
-//        template.setConnectionFactory(jedisConnectionFactory());
-//        return template;
-//    }
-
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
-        // This default constructor utilices hardcode connection properties,
-        //
+        // This default constructor utilices hardcode connection properties
 //        return new LettuceConnectionFactory();
-        String host = "redis";
-        int port = 6379;
+
+        // In docker network we connect using the container name in this case: "redis"
+//        String host = "localhost";
+
+        RedisProperties redisProperties = redisProperties();
+
+        String host = redisProperties.getHost();
+        int port = redisProperties.getPort();
+
+        // improvement, Use another constructor with all the properties
+        // from file property
         return new LettuceConnectionFactory(host, port);
+
+    }
+
+
+    /**
+     * @return
+     */
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate() {
+
+        RedisTemplate<byte[], byte[]> template = new RedisTemplate<>();
+
+        LettuceConnectionFactory redisConnectionFactory = redisConnectionFactory();
+
+        // We log connection properties
+        System.out.println("Database: " + redisConnectionFactory.getDatabase());
+        System.out.println("HostName: " + redisConnectionFactory.getHostName());
+        System.out.println("PassWord: " + redisConnectionFactory.getPassword());
+        System.out.println("Client Name: " + redisConnectionFactory.getClientName());
+        System.out.println("Port: " + redisConnectionFactory.getPort());
+
+        template.setConnectionFactory(redisConnectionFactory);
+        return template;
     }
 
     @Bean
-    public RedisTemplate<?, ?> redisTemplate() {
-        RedisTemplate<byte[], byte[]> template = new RedisTemplate<>();
-        // We log connection properties
-        System.out.println("Database: " + redisConnectionFactory().getDatabase());
-        System.out.println("HostName: " + redisConnectionFactory().getHostName());
-        System.out.println("PassWord: " + redisConnectionFactory().getPassword());
-        System.out.println("Client Name: " + redisConnectionFactory().getClientName());
-        System.out.println("Port: " + redisConnectionFactory().getPort());
-
-        int database = redisConnectionFactory().getDatabase();
-        String hostName = redisConnectionFactory().getHostName();
-        String password = redisConnectionFactory().getPassword();
-        String clientName = redisConnectionFactory().getClientName();
-        int port = redisConnectionFactory().getPort();
-
-//        redisConnectionFactory().setHostName("redis");
-
-        template.setConnectionFactory(redisConnectionFactory());
-        return template;
+    @Primary
+    public RedisProperties redisProperties() {
+        return new RedisProperties();
     }
 
 }
---
+++ up-instance.sh
@@ -0,0 +1,45 @@
+read -r -p "Warning!!! This will clean all images and containers in your local Docker Desktop, Would you like to continue? [y/N] " response
+if [[ "$response" =~ ^([nN])$ ]]
+then
+  exit;
+fi
+
+echo "Clean and launch locally the docker compose"
+echo "------------------------------------------------"
+echo "Kill all running containers"
+docker container kill $(docker ps -q)
+echo "------------------------------------------------"
+echo "Delete all containers that are not running"
+docker container rm $(docker ps -a -q)
+echo "------------------------------------------------"
+echo "Delete all images"
+docker rmi $(docker images -a -q) -f
+echo "------------------------------------------------"
+echo "Build the Jar"
+mvn clean install -Dmaven.test.skip=true
+echo "------------------------------------------------"
+echo "Copy the docker file"
+cp Dockerfile target/Dockerfile
+echo "------------------------------------------------"
+echo "Build the image"
+docker build -t my-store target/
+echo "------------------------------------------------"
+echo "Starting with docker-compose"
+docker-compose up
+# Remove docker image from dockerHub
+#	curl -X DELETE -u "german1982:tato29001807" https://index.docker.io/v1/repositories/german1982/my-store/
+
+# docker image tag my-store german1982/my-store
+# We have to tag the image with german1982/ before!
+#	docker image tag my-store german1982/my-store
+# Upload to docker Hub
+#	docker image push german1982/my-store
+
+############################################################################
+#				Stand alone modes!!!
+#
+#Run redis ( and pull ):
+#	docker run -d -p 6379:6379 redis
+#
+#Docker run image alone:
+#	docker container run -p 8080:8080 -d my-store
